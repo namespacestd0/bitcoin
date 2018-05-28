4bf07e1a83f4f0972eb651aed00f002dd548fc66
Jerry <xiz170@ucsd.edu>
2018-05-27 21:29:44 -0700

    140.83Mhz 217Cycles 27374ALUTS 16607Registers

--- bitcoin_hash.sv
+++ bitcoin_hash.sv
@@ -8,8 +8,7 @@ module bitcoin_hash(input logic clk, reset_n, start,
 	enum logic [3:0]{IDLE, S0, S1, S2, S3, S4, S5, S6, S7, S8} state;
 	
 	logic [15:0] read_address, write_address; // NEXT read or write address
-	logic [15:0] read_to, write_from; // Next read of write index or counter
-	
+	parameter NUM_NONCES = 16;
 	parameter int k[0:63] = '{ // k constants
 		32'h428a2f98,32'h71374491,32'hb5c0fbcf,32'he9b5dba5,32'h3956c25b,32'h59f111f1,32'h923f82a4,32'hab1c5ed5,
 		32'hd807aa98,32'h12835b01,32'h243185be,32'h550c7dc3,32'h72be5d74,32'h80deb1fe,32'h9bdc06a7,32'hc19bf174,
@@ -21,68 +20,42 @@ module bitcoin_hash(input logic clk, reset_n, start,
 		32'h748f82ee,32'h78a5636f,32'h84c87814,32'h8cc70208,32'h90befffa,32'ha4506ceb,32'hbef9a3f7,32'hc67178f2};	
 	parameter int h_const[0:7] = '{
 		32'h6a09e667,32'hbb67ae85,32'h3c6ef372,32'ha54ff53a,32'h510e527f,32'h9b05688c,32'h1f83d9ab,32'h5be0cd19};
-	logic [31:0] w[32], AtoH[8], h[8], h0[8]; 
+	logic [31:0] w[32][NUM_NONCES], AtoH[8][NUM_NONCES], h[8][NUM_NONCES], h0[8]; 
 	logic [31:0] m16, m17, m18; 	// mem[16-18] backup
-	logic [31:0] t0; //temporary values for A-H computation
-	logic [31:0] s1, ch, s0, maj; //temporary values for A-H computation TODO can be removed maybe
+	logic [31:0] t0[NUM_NONCES]; //temporary values for A-H computation
 
 	logic [1:0] round; // round is 0,1,2
 	logic [7:0] t, t_plus_one; // Computation cycle counter range from 0 to 63+1
-	logic [3:0] n; // nonce range from 0 to 15
 
 	logic commonOp;
 
-	function logic [31:0] rrot(input logic [31:0] x, input logic [7:0] r);
-		rrot = (x >> r) | (x << (32-r));
-	endfunction
-	
-	function logic [31:0] wtnew;
+	function logic [31:0] wtnew(input logic [31:0] w1, w14, w0, w9);
 		logic [31:0] s0, s1;
-			s0 = rrot(w[1],7)^rrot(w[1],18)^(w[1]>>3);
-			s1 = rrot(w[14],17)^rrot(w[14],19)^(w[14]>>10);
-			wtnew = w[0] + s0 + w[9] + s1;
-	endfunction
-
-	function logic [127:0] opAtoH_bitOp; // compute s1, s0, ch, maj
-		logic [31:0] s1, ch, s0, maj;
-			s1 					= rrot(AtoH[4], 6) ^ rrot(AtoH[4], 11) ^ rrot(AtoH[4], 25);
-			ch 					= (AtoH[4] & AtoH[5]) ^ ((~AtoH[4]) & AtoH[6]);
-			s0 					= rrot(AtoH[0], 2) ^ rrot(AtoH[0], 13) ^ rrot(AtoH[0], 22);
-			maj 				= (AtoH[0] & AtoH[1]) ^ (AtoH[0] & AtoH[2]) ^ (AtoH[1] & AtoH[2]);
-			opAtoH_bitOp		= {s1, ch, s0, maj};
+			s0 = rightrotate(w1,7)^rightrotate(w1,18)^(w1>>3);
+			s1 = rightrotate(w14,17)^rightrotate(w14,19)^(w14>>10);
+			wtnew = w0 + s0 + w9 + s1;
 	endfunction
 	
-	function logic [255:0] opAtoH;
-		logic [31:0] t1, t2;
-			{s1, ch, s0, maj} 	= opAtoH_bitOp();
-			t1 					= s1 + ch + t0;
-			t2 					= s0 + maj;
-			opAtoH = {t1 + t2, AtoH[0], AtoH[1], AtoH[2], AtoH[3] + t1, AtoH[4], AtoH[5], AtoH[6]};
+	function logic [255:0] sha256_op(input logic [31:0] a, b, c, d, e, f, g, h, t0);
+		logic [31:0] s1, s0, ch, maj, t1, t2; // internal signals
+		begin
+			s1 = rightrotate(e, 6) ^ rightrotate(e, 11) ^ rightrotate(e, 25);
+			ch = (e & f) ^ ((~e) & g);
+			t1 = s1 + ch + t0;
+			s0 = rightrotate(a, 2) ^ rightrotate(a, 13) ^ rightrotate(a, 22);
+			maj = (a & b) ^ (a & c) ^ (b & c);
+			t2 = s0 + maj;
+			sha256_op = {t1 + t2, a, b, c, d + t1, e, f, g};
+		end
 	endfunction
 
+	function logic [31:0] rightrotate(input logic [31:0] x, input logic [7:0] r);
+		begin
+			rightrotate = (x >> r) | (x << (32-r));
+		end
+	endfunction
+	
 	assign mem_clk = clk;
-	// always_ff @(posedge clk) begin
-	// 	case(commonOp)
-	// 		0:begin
-	// 		end
-	// 		1:begin
-	// 			// ** Main Operation **
-	// 			{AtoH[0],AtoH[1],AtoH[2],AtoH[3],AtoH[4],AtoH[5],AtoH[6],AtoH[7]} <= opAtoH();
-	// 			// ** t0 Precomputation**
-	// 			t0		<= AtoH[6] + k[t_plus_one] + w[15];
-	// 			// ** wt PreComputation **
-	// 			for (int n = 0; n < 15; n++) w[n] <= w[n+1];
-	// 			if (t<=15) begin
-	// 				if (round==0)
-	// 					w[15] <= mem_read_data;
-	// 				else
-	// 					for (int m = 15; m < 31; m++) w[m] <= w[m+1];
-	// 			end else begin// t>=15
-	// 				w[15] 	<= wtnew();
-	// 			end
-	// 		end
-	// 	endcase
-	// end
 
 	always_ff @(posedge clk, negedge reset_n) begin
 		if (!reset_n) begin
@@ -90,22 +63,35 @@ module bitcoin_hash(input logic clk, reset_n, start,
 		end else begin
 			if (commonOp) begin
 				// ** Main Operation **
-				{AtoH[0],AtoH[1],AtoH[2],AtoH[3],AtoH[4],AtoH[5],AtoH[6],AtoH[7]} <= opAtoH();
+				for (int n = 0; n < NUM_NONCES; n++) 
+					{AtoH[0][n],AtoH[1][n],AtoH[2][n],AtoH[3][n],AtoH[4][n],AtoH[5][n],AtoH[6][n],AtoH[7][n]} <= sha256_op(AtoH[0][n],AtoH[1][n],AtoH[2][n],AtoH[3][n],AtoH[4][n],AtoH[5][n],AtoH[6][n],AtoH[7][n],t0[n]);
 				// ** t0 Precomputation**
-				t0		<= AtoH[6] + k[t_plus_one] + w[15];
+				for (int n = 0; n < NUM_NONCES; n++) 
+					t0[n]		<= AtoH[6][n] + k[t_plus_one] + w[15][n];
 				// ** wt PreComputation **
-				for (int n = 0; n < 15; n++) w[n] <= w[n+1];
-				if (round ==0 && t<14) w[15] <= mem_read_data;
-				else if (round !=0 && t<14) w[15] <= w[16];
-				else w[15] <= wtnew();
-				for (int m = 16; m < 31; m++) w[m] <= w[m+1];
+				for (int n = 0; n < NUM_NONCES; n++) 
+					for (int i = 0; i < 15; i++) 
+						w[i][n]	 <= w[i+1][n];
+							
+				if (round == 0 && t<14) 
+					w[15][0] <= mem_read_data;
+				else if (round !=0 && t<14) begin
+					for (int n = 0; n < NUM_NONCES; n++) 
+						w[15][n] <= w[16][n];
+				end else begin 
+					for (int n = 0; n < NUM_NONCES; n++) 
+						w[15][n] <= wtnew(w[1][n],w[14][n],w[0][n],w[9][n]);
+				end
+				for (int n = 0; n < NUM_NONCES; n++) 
+					for (int m = 16; m < 31; m++) 
+						w[m][n] <= w[m+1][n];
 
 			end 
 			case (state)
 			IDLE: begin
 				mem_we			<= 0;
-				n				<= 0;
-				for (int i=0; i<8; i++) h[i] <= h_const[i];
+				// n				<= 0;
+				for (int i=0; i<8; i++) h[i][0] <= h_const[i];
 				if (start) begin
 					state 			<= S0;
 					// ** Request mem[0]
@@ -134,13 +120,15 @@ module bitcoin_hash(input logic clk, reset_n, start,
 				mem_addr 		<= read_address; 
 				read_address	<= read_address + 1;
 				// ** Read mem[0]
-				for (int n = 0; n < 15; n++) w[n] <= w[n+1];
-				w[15] <= mem_read_data;
+				for (int n = 0; n < NUM_NONCES; n++) 
+					for (int i = 0; i < 15; i++) 
+						w[i][n]	 <= w[i+1][n];
+				w[15][0] <= mem_read_data;
 			end
 
 			S2: begin 
 				state			<= S3;
-				commonOp	<= 1;
+				commonOp		<= 1;
 				t				<= t_plus_one;
 				t_plus_one		<= t_plus_one + 1;
 				round			<= 0;
@@ -148,12 +136,14 @@ module bitcoin_hash(input logic clk, reset_n, start,
 				mem_addr 		<= read_address; 
 				read_address	<= read_address + 1;
 				// ** Read mem[1]
-				for (int n = 0; n < 15; n++) w[n] <= w[n+1];
-				w[15] 			<= mem_read_data;
+				for (int n = 0; n < NUM_NONCES; n++) 
+					for (int i = 0; i < 15; i++) 
+						w[i][n]	 <= w[i+1][n];
+				w[15][0] 		<= mem_read_data;
 				// ** Precompute t0 of 0
-				t0				<= h[7] + k[0] + w[15];
+				t0[0]				<= h[7][0] + k[0] + w[15][0];
 				// ** Initialize A-H from h0-7
-				for(int i=0;i<8;i++) AtoH[i] <= h[i];
+				for(int i=0;i<8;i++) AtoH[i][0] <= h[i][0];
 			end
 
 			S3: begin
@@ -181,20 +171,22 @@ module bitcoin_hash(input logic clk, reset_n, start,
 				round			<= 1;
 				t				<= t_plus_one;
 				t_plus_one		<= t_plus_one + 1;
+				for (int n = 0; n < NUM_NONCES; n++) begin
 				// ** fill w values
-				w[14]				<= m16;
-				w[15]				<= m17;
-				w[16]				<= m18;
-				w[17]				<= n;						
-				w[18]				<= 32'h80000000;
-				for(int i=19;i<29;i++) w[i] <= 32'h00000000;
-				w[29] 				<= 32'd640;
+					w[14][n]				<= m16;
+					w[15][n]				<= m17;
+					w[16][n]				<= m18;
+					w[17][n]				<= n;						
+					w[18][n]				<= 32'h80000000;
+					for(int i=19;i<29;i++) w[i][n] <= 32'h00000000;
+					w[29][n] 				<= 32'd640;
 				// ** update h0-7 and save a copy
-				for(int i=0;i<8;i++) AtoH[i] <= h[i] + AtoH[i];	// A-H
-				for(int i=0;i<8;i++) h0[i] <= h[i] + AtoH[i];	// extra save
-				for(int i=0;i<8;i++) h[i] <= h[i] + AtoH[i];	// regular update
+					for(int i=0;i<8;i++) AtoH[i][n] <= h[i][0] + AtoH[i][0];	// A-H
+					// for(int i=0;i<8;i++) h0[i] <= h[i] + AtoH[i];	// extra save
+					for(int i=0;i<8;i++) h[i][n] <= h[i][0] + AtoH[i][0];	// regular update
 				// ** Precompute t0 of 0
-				t0					<= h[7] + AtoH[7] + k[0] + m16;
+					t0[n]					<= h[7][0] + AtoH[7][0] + k[0] + m16;
+				end
 			end
 
 			S5: begin
@@ -216,23 +208,25 @@ module bitcoin_hash(input logic clk, reset_n, start,
 				round			<= 2;
 				t				<= t_plus_one;
 				t_plus_one		<= t_plus_one + 1;
+				for (int n = 0; n < NUM_NONCES; n++) begin
 				// ** fill w values
-				for(int i=0;i<8;i++)  w[i+14] <= h[i] + AtoH[i];
-				w[22]			<= 32'h80000000;				
-				for(int j=23;j<29;j++) w[j] <= 32'h00000000;
-				w[29] 			<= 32'd256;
+					for(int i=0;i<8;i++)  w[i+14][n] <= h[i][n] + AtoH[i][n];
+					w[22][n]			<= 32'h80000000;				
+					for(int j=23;j<29;j++) w[j][n] <= 32'h00000000;
+					w[29][n] 			<= 32'd256;
 				// ** update h0-7 nad A-H
-				for (int i=0; i<8; i++) h[i] <= h_const[i];
-				for (int i=0; i<8; i++) AtoH[i] <= h_const[i];
+					for (int i=0; i<8; i++) h[i][n] <= h_const[i];
+					for (int i=0; i<8; i++) AtoH[i][n] <= h_const[i];
 				// ** Precompute t0 of 0
-				t0				<= h_const[7] + k[0] + h[0] + AtoH[0];
+					t0[n]				<= h_const[7] + k[0] + h[0][n] + AtoH[0][n];
+				end
 			end
 
 			S7: begin
 				if (t==63) begin
 					state 			<= S8;
 					commonOp		<= 0;
-					t_plus_one		<= 0;
+					t		<= 0;
 				end else begin
 					// ** t++
 					t				<= t_plus_one;
@@ -241,17 +235,13 @@ module bitcoin_hash(input logic clk, reset_n, start,
 			end
 
 			S8: begin
-				state 			<= IDLE;
-				// commonOp	<= 1;
-				round			<= 3;
-				// t				<= t_plus_one;
-				// t_plus_one		<= t_plus_one + 1;
-				// ** update h0-7 nad A-H
-				for(int i=0;i<8;i++) h[i] <= h[i] + AtoH[i];	// regular update
-				// write
+				if (t==NUM_NONCES-1)
+					state 			<= IDLE;
 				mem_we			<= 1;
 				mem_addr		<= write_address;
-				mem_write_data	<= h[0] + AtoH[0];
+				t				<= t + 1;
+				write_address	<= write_address + 1;
+				mem_write_data	<= h[0][t] + AtoH[0][t];
 			end
 		endcase
 		end
