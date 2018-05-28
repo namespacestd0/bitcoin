2e8c2353c7581ccb52591dd406c9234b0879f335
Jerry <xiz170@ucsd.edu>
2018-05-27 20:26:39 -0700

    1 value working

--- bitcoin_hash.sv
+++ bitcoin_hash.sv
@@ -5,10 +5,7 @@ module bitcoin_hash(input logic clk, reset_n, start,
 	output logic [31:0] mem_write_data,
 	input logic [31:0] mem_read_data);
 	
-	enum logic [3:0]{IDLE, FIRST, SECOND, THIRD, WRITE} state;
-	enum logic [1:0]{PREP, COMPUTE, UPDATE, BOTH} state_mainOp;
-	enum logic [1:0]{READ, WTNEW, FILL640, FILL256} state_w15;
-	enum logic {INIT, COUNT} state_t;
+	enum logic [3:0]{IDLE, S0, S1, S2, S3, S4, S5, S6, S7, S8} state;
 	
 	logic [15:0] read_address, write_address; // NEXT read or write address
 	logic [15:0] read_to, write_from; // Next read of write index or counter
@@ -22,39 +19,19 @@ module bitcoin_hash(input logic clk, reset_n, start,
 		32'ha2bfe8a1,32'ha81a664b,32'hc24b8b70,32'hc76c51a3,32'hd192e819,32'hd6990624,32'hf40e3585,32'h106aa070,
 		32'h19a4c116,32'h1e376c08,32'h2748774c,32'h34b0bcb5,32'h391c0cb3,32'h4ed8aa4a,32'h5b9cca4f,32'h682e6ff3,
 		32'h748f82ee,32'h78a5636f,32'h84c87814,32'h8cc70208,32'h90befffa,32'ha4506ceb,32'hbef9a3f7,32'hc67178f2};	
-	logic [31:0] h[8]; 	// round-end computation value
-	logic [31:0] w[32]; // the latest 16 w values plus buffer values
-	logic [31:0] h0[8]; 	// round1-end computation value
+	parameter int h_const[0:7] = '{
+		32'h6a09e667,32'hbb67ae85,32'h3c6ef372,32'ha54ff53a,32'h510e527f,32'h9b05688c,32'h1f83d9ab,32'h5be0cd19};
+	logic [31:0] w[32], AtoH[8], h[8], h0[8]; 
 	logic [31:0] m16, m17, m18; 	// mem[16-18] backup
-	// logic [31:0] A, B, C, D, E, F, G, H;
-	logic [31:0] AtoH[8];
-	// NEW
-	// logic [31:0] read_buffer;
-
 	logic [31:0] t0; //temporary values for A-H computation
-	logic [31:0] s1, ch, s0, maj; //temporary values for A-H computation
+	logic [31:0] s1, ch, s0, maj; //temporary values for A-H computation TODO can be removed maybe
 
 	logic [1:0] round; // round is 0,1,2
-	logic [8:0] t; // Computation cycle counter range from 0 to 63+1
-	logic [8:0] t_plus_one; // performance-wise t+1 tracking register
+	logic [7:0] t, t_plus_one; // Computation cycle counter range from 0 to 63+1
 	logic [3:0] n; // nonce range from 0 to 15
 
-	logic saved;
+	logic commonOp;
 
-	logic wtread;
-	// logic sw_read;		// request mem[read_address], w[read_to]=mem_read_data
-	// logic sw_wtmem;		// update w[0-15]
-	// logic sw_t0;		// pre-compute t0 of t from mem_read_data, or w[15]
-	logic [31:0] wt;	// switching wire for above
-	// logic [15:0] last_t; // break out condition
-	// assign A = AtoH[0];
-	// assign B = AtoH[1];
-	// assign C = AtoH[2];
-	// assign D = AtoH[3];
-	// assign E = AtoH[4];
-	// assign F = AtoH[5];
-	// assign G = AtoH[6];
-	// assign H = AtoH[7];
 	function logic [31:0] rrot(input logic [31:0] x, input logic [7:0] r);
 		rrot = (x >> r) | (x << (32-r));
 	endfunction
@@ -84,177 +61,199 @@ module bitcoin_hash(input logic clk, reset_n, start,
 	endfunction
 
 	assign mem_clk = clk;
-	always_ff @(posedge clk) begin // A-H <= ?
-		case (state_mainOp)
-			PREP:
-				for(int i=0;i<8;i++) AtoH[i] <= h[i];
-			COMPUTE:
-				{AtoH[0],AtoH[1],AtoH[2],AtoH[3],AtoH[4],AtoH[5],AtoH[6],AtoH[7]} <= opAtoH();
-			UPDATE:	begin											
-				for(int i=0;i<8;i++) AtoH[i] <= h[i] + AtoH[i];	
-				if (!saved) for(int i=0;i<8;i++) h0[i] <= h[i] + AtoH[i];
-				saved <= 1;
-			end
-			BOTH: begin
-				for(int i=0;i<8;i++) AtoH[i] <= h[i];
-				
-			end
-		endcase
-	end
-	always_ff @(posedge clk) begin // w[15] <= ?
-		case (state_w15)
-			READ: begin
-				// w[0-14]
-				for (int n = 0; n < 15; n++) w[n] <= w[n+1];
-				// w[15]
-				if (round == 0) w[15] <= mem_read_data; // read from memory
-				else for (int m = 15; m < 31; m++) w[m] <= w[m+1]; // read from buffer
-			end
-			WTNEW: begin
-				for (int n = 0; n < 15; n++) w[n] <= w[n+1];
-				w[15] 				<= wtnew();
-				if (round==0) begin
-					if (t_plus_one == 15) m16 <= mem_read_data;
-					if (t_plus_one == 16) m17 <= mem_read_data;
-					if (t_plus_one == 17) m18 <= mem_read_data;
-				end			
-			end
-			FILL640: begin
-				w[15+1]				<= m16;
-				w[16+1]				<= m17;
-				w[17+1]				<= m18;
-				w[18+1]				<= n;						
-				w[19+1]				<= 32'h80000000;
-				for(int i=21;i<31;i++) w[i] <= 32'h00000000;
-				w[30+1] 			<= 32'd640;			
-			end
-			FILL256: begin
-				// for(int i=16;i<24;i++)  w[i] <= h0[i] + AtoH[i];
-				w[24]				<= 32'h80000000;				
-				for(int j=25;j<31;j++) w[j] <= 32'h00000000;
-				w[31] 				<= 32'd256;
-			end
-		endcase
-	end
-	always_ff @(posedge clk) begin // t0 <= ?
-		if (t_plus_one==0)
-			if (round==1)
-				if (saved)
-					t0		<= h0[7] + AtoH[7] + k[t_plus_one] + w[15];
-				else
-					t0		<= h[7] + AtoH[7] + k[t_plus_one] + w[15];
-			else
-				t0		<= h[7] + k[t_plus_one] + w[15];
-		else
-			t0		<= AtoH[6] + k[t_plus_one] + w[15];
-	end
-	always_ff @(posedge clk) begin // t counter
-		case (state_t)
-			INIT: begin
-				t			<= 63;
-				t_plus_one	<= 64;
-			end
-			COUNT: begin
-				t			<= t_plus_one;
-				if (t_plus_one == 64)
-					t_plus_one	<= 0;
-				else t_plus_one	<= t_plus_one + 1;
-			end
-		endcase
-	end
+	// always_ff @(posedge clk) begin
+	// 	case(commonOp)
+	// 		0:begin
+	// 		end
+	// 		1:begin
+	// 			// ** Main Operation **
+	// 			{AtoH[0],AtoH[1],AtoH[2],AtoH[3],AtoH[4],AtoH[5],AtoH[6],AtoH[7]} <= opAtoH();
+	// 			// ** t0 Precomputation**
+	// 			t0		<= AtoH[6] + k[t_plus_one] + w[15];
+	// 			// ** wt PreComputation **
+	// 			for (int n = 0; n < 15; n++) w[n] <= w[n+1];
+	// 			if (t<=15) begin
+	// 				if (round==0)
+	// 					w[15] <= mem_read_data;
+	// 				else
+	// 					for (int m = 15; m < 31; m++) w[m] <= w[m+1];
+	// 			end else begin// t>=15
+	// 				w[15] 	<= wtnew();
+	// 			end
+	// 		end
+	// 	endcase
+	// end
+
 	always_ff @(posedge clk, negedge reset_n) begin
 		if (!reset_n) begin
 			state <= IDLE;
-		end else case (state)
-			IDLE: begin
-				// reset
-				mem_we			<= 0;
-				h[0] <= 32'h6a09e667;
-				h[1] <= 32'hbb67ae85;
-				h[2] <= 32'h3c6ef372;
-				h[3] <= 32'ha54ff53a;
-				h[4] <= 32'h510e527f;
-				h[5] <= 32'h9b05688c;
-				h[6] <= 32'h1f83d9ab;
-				h[7] <= 32'h5be0cd19;
+		end else begin
+			if (commonOp) begin
+				// ** Main Operation **
+				{AtoH[0],AtoH[1],AtoH[2],AtoH[3],AtoH[4],AtoH[5],AtoH[6],AtoH[7]} <= opAtoH();
+				// ** t0 Precomputation**
+				t0		<= AtoH[6] + k[t_plus_one] + w[15];
+				// ** wt PreComputation **
+				for (int n = 0; n < 15; n++) w[n] <= w[n+1];
+				if (round ==0 && t<14) w[15] <= mem_read_data;
+				else if (round !=0 && t<14) w[15] <= w[16];
+				else w[15] <= wtnew();
+				for (int m = 16; m < 31; m++) w[m] <= w[m+1];
 
+			end 
+			case (state)
+			IDLE: begin
+				mem_we			<= 0;
+				n				<= 0;
+				for (int i=0; i<8; i++) h[i] <= h_const[i];
 				if (start) begin
-					state 			<= FIRST;
-					state_t			<= INIT;
-					round 			<= 0;
-					done			<= 0;
-					n				<= 0;
-					// **request mem[0]**
+					state 			<= S0;
+					// ** Request mem[0]
 					mem_addr 		<= message_addr; 
-					// **prepare for future**
+					// ** Prepare for future
 					read_address	<= message_addr + 1;
 					write_address	<= output_addr;
+					done			<= 0;
 				end else begin
 					done			<= 1;
 				end
 			end
 
-			FIRST: begin
-				state_t			<= COUNT;
-				if (read_address == 1)
-					state_w15	<= READ;
-				else if (t==13)
-					state_w15	<= WTNEW;
-				else if (t==61) begin
-					state		<= SECOND;
-					state_w15	<= FILL640;
-					round		<= 1;
-				end else if (t==63) begin
-					state_mainOp<= PREP;
-				end else if (t==64) begin
-					state_mainOp<= COMPUTE;
-				end
-
-				if (read_address <= 18) begin
-					mem_addr 		<= read_address; 
-					read_address	<= read_address + 1;
-				end
+			S0: begin
+				state			<= S1;
+				// ** Request mem[1]
+				mem_addr 		<= read_address; 
+				read_address	<= read_address + 1;
 			end
 
-			SECOND: begin
-				if (t==13) begin
-					state_w15	<= WTNEW;
-				end
-				if (t==61) begin
-					state		<= THIRD;
-					round		<= 2;
-					state_w15	<= FILL256;
-				end
-				if (t==62) begin
-					state_w15	<= READ;
-				end
+			S1: begin
+				state			<= S2;
+				commonOp		<= 0;
+				t_plus_one		<= 0;
+				// ** Request mem[2]
+				mem_addr 		<= read_address; 
+				read_address	<= read_address + 1;
+				// ** Read mem[0]
+				for (int n = 0; n < 15; n++) w[n] <= w[n+1];
+				w[15] <= mem_read_data;
+			end
+
+			S2: begin 
+				state			<= S3;
+				commonOp	<= 1;
+				t				<= t_plus_one;
+				t_plus_one		<= t_plus_one + 1;
+				round			<= 0;
+				// ** Request mem[3]
+				mem_addr 		<= read_address; 
+				read_address	<= read_address + 1;
+				// ** Read mem[1]
+				for (int n = 0; n < 15; n++) w[n] <= w[n+1];
+				w[15] 			<= mem_read_data;
+				// ** Precompute t0 of 0
+				t0				<= h[7] + k[0] + w[15];
+				// ** Initialize A-H from h0-7
+				for(int i=0;i<8;i++) AtoH[i] <= h[i];
+			end
+
+			S3: begin
 				if (t==63) begin
-					state_mainOp<= UPDATE;
-					state_w15	<= READ;
+					state 			<= S4;
+					commonOp		<= 0;
+					t_plus_one		<= 0;
+				end else begin
+					// ** t++
+					t				<= t_plus_one;
+					t_plus_one		<= t_plus_one + 1;
 				end
-				if (t==64) begin
-					state_mainOp<= COMPUTE;	
-				end
+				// ** Request new mem[read_address]
+				mem_addr 		<= read_address; 
+				read_address	<= read_address + 1;
+				// ** Backup w[16-18]
+				if (t_plus_one == 15) m16 <= mem_read_data;
+				if (t_plus_one == 16) m17 <= mem_read_data;
+				if (t_plus_one == 17) m18 <= mem_read_data;
 			end
-			
-			THIRD: begin
-				if (t==13) begin
-					state_w15	<= WTNEW;
-				end
-				if (t==62) begin
-					state_mainOp<= UPDATE;
-					state_w15	<= READ;
-				end
 
+			S4: begin // ** major transition
+				state 			<= S5;
+				commonOp		<= 1;
+				round			<= 1;
+				t				<= t_plus_one;
+				t_plus_one		<= t_plus_one + 1;
+				// ** fill w values
+				w[14]				<= m16;
+				w[15]				<= m17;
+				w[16]				<= m18;
+				w[17]				<= n;						
+				w[18]				<= 32'h80000000;
+				for(int i=19;i<29;i++) w[i] <= 32'h00000000;
+				w[29] 				<= 32'd640;
+				// ** update h0-7 and save a copy
+				for(int i=0;i<8;i++) AtoH[i] <= h[i] + AtoH[i];	// A-H
+				for(int i=0;i<8;i++) h0[i] <= h[i] + AtoH[i];	// extra save
+				for(int i=0;i<8;i++) h[i] <= h[i] + AtoH[i];	// regular update
+				// ** Precompute t0 of 0
+				t0					<= h[7] + AtoH[7] + k[0] + m16;
+			end
+
+			S5: begin
 				if (t==63) begin
-					state_mainOp<= PREP;
-					state_w15	<= READ;
-				end
-				if (t==64) begin
-					state_mainOp<= COMPUTE;	
+					state 			<= S6;
+					commonOp		<= 0;
+					t_plus_one		<= 0;
+					
+				end else begin
+					// ** t++
+					t				<= t_plus_one;
+					t_plus_one		<= t_plus_one + 1;
 				end
 			end
-	endcase
+
+			S6: begin // ** major transition
+				state 			<= S7;
+				commonOp	<= 1;
+				round			<= 2;
+				t				<= t_plus_one;
+				t_plus_one		<= t_plus_one + 1;
+				// ** fill w values
+				for(int i=0;i<8;i++)  w[i+14] <= h[i] + AtoH[i];
+				w[22]			<= 32'h80000000;				
+				for(int j=23;j<29;j++) w[j] <= 32'h00000000;
+				w[29] 			<= 32'd256;
+				// ** update h0-7 nad A-H
+				for (int i=0; i<8; i++) h[i] <= h_const[i];
+				for (int i=0; i<8; i++) AtoH[i] <= h_const[i];
+				// ** Precompute t0 of 0
+				t0				<= h_const[7] + k[0] + h[0] + AtoH[0];
+			end
+
+			S7: begin
+				if (t==63) begin
+					state 			<= S8;
+					commonOp		<= 0;
+					t_plus_one		<= 0;
+				end else begin
+					// ** t++
+					t				<= t_plus_one;
+					t_plus_one		<= t_plus_one + 1;
+				end
+			end
+
+			S8: begin
+				state 			<= IDLE;
+				// commonOp	<= 1;
+				round			<= 3;
+				// t				<= t_plus_one;
+				// t_plus_one		<= t_plus_one + 1;
+				// ** update h0-7 nad A-H
+				for(int i=0;i<8;i++) h[i] <= h[i] + AtoH[i];	// regular update
+				// write
+				mem_we			<= 1;
+				mem_addr		<= write_address;
+				mem_write_data	<= h[0] + AtoH[0];
+			end
+		endcase
+		end
 	end
- 
 endmodule
